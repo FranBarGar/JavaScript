6bcfa0f3826eb65673fb2e1c8829032479e7bbb3
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-09-23 20:32:19 +0200

    q11 changes

--- q11/script.js
+++ q11/script.js
@@ -10,6 +10,7 @@ if (num && !isNaN(parseFloat(num))) {
     sum += parseInt(num[i]);
   }
   document.write("Modificacion 1: " + num + " = " + sum + "<br>");
+
   sum = 0;
   for (var i = 0; i < length; i++) {
     if (!isNaN(num[i])) {
@@ -17,6 +18,22 @@ if (num && !isNaN(parseFloat(num))) {
     }
   }
   document.write("Modificacion 2: " + num + " = " + sum + "<br>");
+
+  sum = 0;
+  var sumDec = 0;
+  for (var i = 0; i < length; i++) {
+    if (!isNaN(num[i])) {
+      sum += parseInt(num[i]);
+    } else {
+      for (var j = i+1; j < length; j++) {
+        sumDec += parseInt(num[j])*0.1;
+      }
+      sum += sumDec;
+      break;
+    }
+  }
+  document.write("Modificacion 2(alt): " + num + " = " + sum + "<br>");
+
 } else {
   document.write("El valor introducido no es un numero.")
 }
