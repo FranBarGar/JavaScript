274afb5f59f887782119cfb6313c44fd9a39be1f
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-09-25 16:57:49 +0200

    Correccion en clase

--- Unit2/q07/script.js
+++ Unit2/q07/script.js
@@ -1,6 +1,6 @@
 var num = prompt("Introduzca un numero menor a 1000.");
 
-if (num && !isNaN(num) && num>-1000 && num<1000) {
+if (num && !isNaN(num) && num>0 && num<1000) {
   var numLength = num.length;
   for (var i = 0; i < numLength; i++) {
     alert(num[i]);
--- Unit2/q11/script.js
+++ Unit2/q11/script.js
@@ -19,21 +19,6 @@ if (num && !isNaN(num)) {
   }
   document.write("Modificacion 2: " + num + " = " + sum + "<br>");
 
-  sum = 0;
-  var sumDec = 0;
-  for (var i = 0; i < length; i++) {
-    if (!isNaN(num[i])) {
-      sum += parseInt(num[i]);
-    } else {
-      for (var j = i+1; j < length; j++) {
-        sumDec += parseInt(num[j])*0.1;
-      }
-      sum += sumDec;
-      break;
-    }
-  }
-  document.write("Modificacion 2(alt): " + num + " = " + sum + "<br>");
-
 } else {
   document.write("El valor introducido no es un numero.")
 }
