bbfcdb46a786b095b4e217a0a536a82f1e7e6bf5
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-10-03 17:18:01 +0200

    Correcciones varias

--- Unit3/q20/script.js
+++ Unit3/q20/script.js
@@ -1,4 +1,4 @@
-var regExp = /^\d{4}[b-df-hj-npr-tv-z]$/i;
+var regExp = /^\d{4}[b-df-hj-npr-tv-z]{3}$/i;
 var matr = prompt(`Introduzca una matricula moderna: `);
 
 document.write(regExp.test(matr)?`Matricula correcta: ${matr}`:`Matricula incorrecta: ${matr}`);
--- Unit3/q21/script.js
+++ Unit3/q21/script.js
@@ -1,4 +1,4 @@
-var regExp = /^(Lunes|Martes|Miercoles|Jueves|Viernes|Sabado|Domingo) (([0-2]\d)|([3][01]))$/i;
+var regExp = /^(Lunes|Martes|Miercoles|Jueves|Viernes|Sabado|Domingo) (([0][1-9])|([1-2]\d)|([3][01]))$/i;
 var day = prompt(`Introduzca el nombre de un dia y 2 digitos: `);
 
 document.write(regExp.test(day)?`Dia correcto: ${day}`:`Dia incorrecto: ${day}<br>Se debe introducir un dia valido: diaSemana diaMes.`);
