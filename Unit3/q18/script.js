9fc6a3067e4d861553ba8674c304ef98db925eb3
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-10-04 17:23:56 +0200

    Urls

--- Unit3/q18/script.js
+++ Unit3/q18/script.js
@@ -1,4 +1,4 @@
-var regExp = /^\d{1,3}\.\d{1,3}$/
+var regExp = /^\(\d{1,3}\.\d{1,3}\)$/
 var coord = prompt(`Introduzca una coordenada: `);
 
-document.write(regExp.test(coord)?`Coordenada correcta: ${coord}`:`Coordenada incorrecta: ${coord}`);
+document.write(regExp.test(coord)?`Coordenada correcta: ${coord}`:`Coordenada incorrecta: ${coord}<br>Debe introducir una coordenada del estilo 999.999`);
