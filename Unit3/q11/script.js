d59105fde94b7c26cf0b5e6a74a8a81be93172c0
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-10-11 10:27:35 +0200

    q11&13

--- Unit3/q11/script.js
+++ Unit3/q11/script.js
@@ -1,4 +1,4 @@
-var num1, num2;
+var num1, num2, num3;
 var exit = false;
 do {
   calc();
@@ -38,7 +38,8 @@ do {
     break;
     case "9":
     paramBase();
-    console.log("Cambio a base 10:  "+num1+"//"+num2+"="+ parseInt(num1, num2)+"\n");
+    let aux = parseInt(num2, num1);
+    console.log(`Cambio de base ${num1} a base ${num3}: \n${num2} de base ${num1} a base ${num3} = ${aux.toString(num3)}\n`);
     break;
     case "10":
     exit = confirm("¿Desea salir de la calculadora?");
@@ -58,7 +59,7 @@ function calc() {
   +"Pulse 6 para calcular la raiz cuadrada\n"
   +"Pulse 7 para calcular la potencia\n"
   +"Pulse 8 para calcular el logaritmo neperiano\n"
-  +"Pulse 9 para cambiar a base 10\n"
+  +"Pulse 9 para calcular de una base dada a otra base deseada\n"
   +"Pulse 10 para salir\n");
 }
 
@@ -84,13 +85,28 @@ function param2() {
 }
 
 function paramBase() {
+  var regExp1 = /^([2-9]|([1-2][0-9])|(3[0-6]))$/mg;
   do {
-    num1 = prompt("Introduzca la primera variable: ");
-  } while (!num1);
+    num1 = prompt("Introduzca la base del numero que va a introducir: ");
+  } while (isNaN(num1) || !num1 || !regExp1.test(num1));
+
+  var regExp2;
+  var aux = parseInt(num1)-1;
+  var alph = "0123456789abcdefghijklmnopqrstuvwxyz";
+  if (aux>=10) {
+    regExp2 = new RegExp(`^([+-]?([0-9]+[a-${alph[aux]}]*)|([0-9]*[a-${alph[aux]}]+))$`, 'img');
+  } else {
+    regExp2 = new RegExp(`^([+-]?[0-${aux}]+)$`, 'mg');
+  }
+  num1 = Number(num1);
 
   do {
-    num2 = prompt("Introduzca la segunda variable: ");
-  } while (isNaN(num2) && !num2);
-  num2 = Number(num2);
-  console.log("Variable 1 = "+num1+"\nVariable 2 = "+num2+"\n");
+    num2 = prompt("Introduzca el numero: ");
+  } while (!num2 || !regExp2.test(num2));
+
+  do {
+    num3 = prompt("Introduzca la base deseada: ");
+  } while (isNaN(num3) || !num3 || regExp1.test(num3));
+  num3 = Number(num3);
+  console.log("Numero = "+num2+"\nBase del numero = "+num1+"\nBase deseada = "+num3+"\n");
 }
