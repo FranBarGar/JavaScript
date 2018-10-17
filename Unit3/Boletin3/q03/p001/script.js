c5a2093b4b64f322c48b514a90203ff106eba057
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-10-17 19:34:26 +0200

    liandola.html

--- Unit3/Boletin3/q03/p001/script.js
+++ Unit3/Boletin3/q03/p001/script.js
@@ -1,10 +1,12 @@
+var regExp = /^([-+]?[0-9]+)$/img;
+
 function calc() {
-  var regExp = /^[-+]?[1-9][0-9]*$/img;
-  var base = window.frames[0].document.forms[0].elements[0].value;
-  var exp = window.frames[0].document.forms[0].elements[1].value
-  if (regExp.test(base) && regExp.test(exp)) {
+  var base = parseInt(window.frames[0].document.forms[0].elements[0].value);
+  var exp = parseInt(window.frames[0].document.forms[0].elements[1].value);
+  document.forms[0].elements[1].value = Math.pow(base, exp);
+  /*if (regExp.test(exp) && regExp.test(base)) {
     document.forms[0].elements[1].value = Math.pow(base, exp);
   } else {
-    alert(`Algun numero no esta bien formado.`)
-  }
+    alert(`Algun numero no esta bien formado.`);
+  }*/
 }
