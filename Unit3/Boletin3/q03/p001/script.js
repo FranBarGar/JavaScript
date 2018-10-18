ef4d32e60eab38ede422478e49ffe7ba8a2d2d14
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-10-18 02:39:50 +0200

    puta g

--- Unit3/Boletin3/q03/p001/script.js
+++ Unit3/Boletin3/q03/p001/script.js
@@ -1,12 +1,12 @@
-var regExp = /^([-+]?[0-9]+)$/img;
+var regExp = /^([-+]?[0-9]+)$/;
 
 function calc() {
   var base = parseInt(window.frames[0].document.forms[0].elements[0].value);
   var exp = parseInt(window.frames[0].document.forms[0].elements[1].value);
   document.forms[0].elements[1].value = Math.pow(base, exp);
-  /*if (regExp.test(exp) && regExp.test(base)) {
+  if (regExp.test(exp) && regExp.test(base)) {
     document.forms[0].elements[1].value = Math.pow(base, exp);
   } else {
     alert(`Algun numero no esta bien formado.`);
-  }*/
+  }
 }
--- Unit3/Boletin3/q03/p002/script.js
+++ Unit3/Boletin3/q03/p002/script.js
@@ -1,11 +1,10 @@
 function calc() {
-  var regExp = /^[-+]?[0-9]+$/img;
-  var base = parseInt(document.forms[0].elements[0].value);
-  var exp = parseInt(document.forms[0].elements[1].value);
-  window.frames[0].document.forms[0].elements[1].value;
-  /*if (regExp.test(base) || regExp.test(exp)) {
+  var regExp = /^[-+]?[0-9]+$/;
+  var base = parseInt(parent.document.forms[0].elements[0].value);
+  var exp = parseInt(parent.document.forms[0].elements[1].value);
+  if (regExp.test(base) && regExp.test(exp)) {
     document.forms[0].elements[1].value = Math.pow(base, exp);
   } else {
-    alert(`Algun numero no esta bien formado.`)
-  }*/
+    alert(`Error: Algun numero no esta bien formado.`)
+  }
 }
