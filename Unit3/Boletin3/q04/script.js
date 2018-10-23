b2b4c7151539320fcf380c8f57da763582144247
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-10-23 17:14:26 +0200

    Correciones

--- Unit3/Boletin3/q04/script.js
+++ Unit3/Boletin3/q04/script.js
@@ -1,22 +1,20 @@
 var ar = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-function main() {
-  for (var i = 0; i < 10; i++) {
-    throwDices();
-  }
-
-  // document.write(form);
-  for (var i = 0; i < ar.length; i++) {
-    // document.write(`<p>El resultado ${i+2} ha salido: ${ar[i]} veces</p>`);
-    console.log(`El resultado ${i+2} ha salido: ${ar[i]} veces`);
-    console.log(`\n`);
-  }
-}
-
-function throwDices() {
-  var d1 = Math.floor((Math.random()*6)+1);
-  var d2 = Math.floor((Math.random()*6)+1);
-  document.forms[0].elements[0].value = d1;
-  document.forms[0].elements[1].value = d2;
-  ar[d1+d2-2]++;
+function throwXDices(x) {
+    if (x != 0) {
+        var d1 = Math.floor((Math.random()*6)+1);
+        var d2 = Math.floor((Math.random()*6)+1);
+        document.forms[0].elements[0].value = d1;
+        document.forms[0].elements[1].value = d2;
+        ar[d1+d2-2]++;
+        setTimeout(function() {
+            throwXDices(--x);
+        }, 1000);
+    } else {
+        for (var i = 0; i < ar.length; i++) {
+            // document.write(`<p>El resultado ${i+2} ha salido: ${ar[i]} veces</p>`); Tema 6
+            console.log(`El resultado ${i+2} ha salido: ${ar[i]} veces`);
+            console.log(`\n`);
+        }
+    }
 }
