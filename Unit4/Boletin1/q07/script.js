90a9ed0bb704549877f0ac61f279c8e630f19e66
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-10-25 22:49:39 +0200

    Tareas con diccionario ingles

--- Unit4/Boletin1/q07/script.js
+++ Unit4/Boletin1/q07/script.js
@@ -5,18 +5,24 @@ var w, clearStr = [];
 function introducirPalabras() {
     var str = prompt(`Introduzca palabras separadas por coma.`).split(",");
     for (var i = 0; i < str.length; i++) {
-            if (/\w+/i.test(str[i])) {
-                clearStr.push(str[i]);
-            }
+        if (/\w+/i.test(str[i])) {
+            clearStr.push(str[i]);
+        }
     }
     crearLista(...clearStr);
 }
 
 function crearLista(...str) {
-    w = open("./q07b.html", "List", "width=500, height=300");
     if (str.length != 0) {
-        for (var i = 0; i < str.length; i++) {
-            w.list.innerHTML += `<li>${str[i]}</li>`;
-        }
+        w = open("./q07b.html", "List", "width=500, height=300");
+        setTimeout(function() {
+            w.body.innerHTML += `Todas las palabras.<br>${str.join(" - ")}<br><br>`;
+            w.body.innerHTML += `Todas las palabras colocadas al revés. <br>${str.reverse().join(" - ")}<br><br>`;
+            w.body.innerHTML += `La primera palabra ingresada por el usuario.<br>${str[0]}<br><br>`;
+            w.body.innerHTML += `La última palabra ingresada por el usuario.<br>${str[str.length-1]}<br><br>`;
+            w.body.innerHTML += `El número de palabras presentes en el array.<br>${str.length}<br><br>`;
+            w.body.innerHTML += `Las palabras ordenadas de la 'a' a la 'z'.<br>${str.sort().join(" - ")}<br><br>`;
+            w.body.innerHTML += `Las palabras Ordenadas de la 'z' a la 'a'.<br>${str.sort().reverse().join(" - ")}<br><br>`;
+        }, 500);
     }
 }
