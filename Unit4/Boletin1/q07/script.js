17c2216ed7f9c46a0a37f889a667cfd9e3fd1acf
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-10-25 19:06:21 +0200

    intento

--- Unit4/Boletin1/q07/script.js
+++ Unit4/Boletin1/q07/script.js
@@ -1,15 +1,22 @@
-var clearStr = [];
+var w, clearStr = [];
+
+
 
 function introducirPalabras() {
     var str = prompt(`Introduzca palabras separadas por coma.`).split(",");
     for (var i = 0; i < str.length; i++) {
-            if (str[i]) {
+            if (/\w+/i.test(str[i])) {
                 clearStr.push(str[i]);
             }
     }
     crearLista(...clearStr);
 }
 
-function crearLista() {
-
+function crearLista(...str) {
+    w = open("./q07b.html", "List", "width=500, height=300");
+    if (str.length != 0) {
+        for (var i = 0; i < str.length; i++) {
+            w.list.innerHTML += `<li>${str[i]}</li>`;
+        }
+    }
 }
--- Unit4/Boletin1/q08/script.js
+++ Unit4/Boletin1/q08/script.js
@@ -1,3 +1,4 @@
 var valores = [true, 5, false, "hola", "adios", 2];
 valores.sort();
 console.log(valores);
+// Lo transforma todo a cadena y lo ordena, no respeta el tipo.
