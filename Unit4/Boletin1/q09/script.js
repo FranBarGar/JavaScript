b5161d0519915278d07f81d491719b7763511f20
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-10-30 17:19:01 +0100

    Correciones

--- Unit4/Boletin1/q09/script.js
+++ Unit4/Boletin1/q09/script.js
@@ -1,28 +1,40 @@
-var ar = [];
-var asignaturas = ["DAWEB", "DWEC", "DWES"]
+var asignaturas = ["DAWEB", "DWEC", "DWES"];
 
-for (i = 0; i < 5; i++) {
-    var alumno = [];
+mostrarTabla(pedirAlumnos(5));
+
+function pedirNotas(alumno) {
+    for (var i = 0; i < 3; i++) {
+        do {
+            var nota = prompt(`Introduzca la nota de el alumno en el modulo ${asignaturas[i]}: `);
+        } while (!(/^(([0-9])|(10))$/.test(nota)));
+        alumno.push(nota)
+    }
+}
+
+function pedirNombre(alumno) {
     do {
         var nombre = prompt(`Introduzca el nombre de el alumno: `);
     } while (!(/^[a-znáéíóú]+$/i.test(nombre)));
     alumno.push(nombre);
-    for (m = 0; m < 3; m++) {
-        do {
-            var nota = prompt(`Introduzca la nota de el alumno en el modulo ${asignaturas[m]}: `);
-        } while (!(/^(([0-9])|(10))$/.test(nota)));
-        alumno.push(nota)
-    }
-    ar.push(alumno);
 }
 
-for (var i = 0; i < ar.length; i++) {
-        tabla.innerHTML+=`
-        <tr>
-        <td>${ar[i][0]}</td>
-        <td>${ar[i][1]}</td>
-        <td>${ar[i][2]}</td>
-        <td>${ar[i][3]}</td>
-        </tr>`;
+function pedirAlumnos(num) {
+    var ar = [];
+    for (i = 0; i < num; i++) {
+        var alumno = [];
+        pedirNombre(alumno);
+        pedirNotas(alumno);
+        ar.push(alumno);
+    }
+    return ar;
+}
 
+function mostrarTabla(ar) {
+    for (var i = 0; i < ar.length; i++) {
+        tabla.innerHTML+=`<tr id="${i}">`;
+        for (var j = 0; j < ar[i].length; j++) {
+            document.getElementById(`${i}`).innerHTML+=`<td>${ar[i][j]}</td>`;
+        }
+        tabla.innerHTML+=`<tr>`;
+    }
 }
