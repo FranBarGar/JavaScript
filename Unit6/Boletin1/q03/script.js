6a87590837b6a79cc89c436955beff47b49b81c7
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-12-05 03:11:34 +0100

    Tarea

--- Unit6/Boletin1/q03/script.js
+++ Unit6/Boletin1/q03/script.js
@@ -4,5 +4,19 @@ window.onload = function(){
         var text = document.createTextNode(input.value);
         li.appendChild(text);
         ul.appendChild(li);
+        input.value = "";
+    });
+    eliminar.addEventListener('click', function(){
+        var lis = document.getElementsByTagName('li');
+        for (var i = 0; i < lis.length; i++) {
+            if (lis[i].firstChild.data.trim() == "") {
+                ul.removeChild(lis[i])
+                i--;
+            }
+        }
+    });
+    duplicar.addEventListener('click', function(){
+        var node = ul.lastChild.cloneNode(true);
+        ul.appendChild(node);
     });
 };
