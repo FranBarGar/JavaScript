0f0840c89d8caa7813e3ad0c0487f73b8dfc2a8d
FranciscoBarbaG <francisco.barba@iesdonana.org>
2019-02-05 17:24:11 +0100

    Actividades

---
+++ Unit7/1/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    var $h2 = $('h2');
+    console.log($h2);
+    $('.details').html('$199.99');
+    $h2.html('Sanlúcar de Barrameda');
+    console.log($('#vacations'));
+    console.log($('.america'));
+});
