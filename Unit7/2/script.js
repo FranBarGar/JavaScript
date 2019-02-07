c8b3edd10deeecb2dd6cb651fd086b91fd7c8c5a
FranciscoBarbaG <francisco.barba@iesdonana.org>
2019-02-07 02:48:20 +0100

    Ultima actividad

--- Unit7/2/script.js
+++ Unit7/2/script.js
@@ -1,13 +1,34 @@
 $(function(){
-    // 1
+    //a
     console.log($('p:contains("Primero") + ul li:not(:empty)'));
+    console.log($('ul:first li:not(:empty)'));
+
+    //b
+    console.log($('li:contains("Sistemas Informáticos")'));
+    console.log($('ul:first li:not(:empty):eq(2)'));
+
+    //c
+    console.log($('li:contains("Sistemas Informáticos")').next());
+    console.log($('ul:first li:eq(2) + li'));
+
+    //d
+    console.log($('ul:first li:eq(2) ~ li:not(:empty)'));
 
 
-    //2
-    console.log($('a:contains("Sistemas Informáticos")'));
+    //e
+    console.log($('[href]'));
 
 
-    //3
-    
+    //f
+    console.log($(':not([href])'));
+
+
+    //g
+    console.log($(':contains("mod")'));
+
+
+    //h
+    console.log($(':contains("mod")'));
+
 
 });
