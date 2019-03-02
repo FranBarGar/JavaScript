655b905fe7f4d9612817a8b96af6f1ad67d21883
FranciscoBarbaG <francisco.barba@iesdonana.org>
2019-03-02 03:08:21 +0100

    nose

--- Unit8/6/T01/js/script.js
+++ Unit8/6/T01/js/script.js
@@ -1,7 +1,39 @@
 $(()=>{
-    $('#date').datepicker();
-    $('#date').hover((e) => {
-        $('<p class="selector"></p>')
+    fecha();
+    sexo();
+});
+
+function fecha() {
+    $.datepicker.regional['es'] = {
+        closeText: 'Cerrar',
+        prevText: '< Ant',
+        nextText: 'Sig >',
+        currentText: 'Hoy',
+        monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+        dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+        dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+        weekHeader: 'Sm',
+        dateFormat: 'dd/mm/yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['es']);
+
+    $('#date').datepicker({
+        showAnim: "slideDown",
+        minDate: '-12y',
+        maxDate: new Date(),
+    });
+
+    $('<img>')
+    .attr('src', 'images/calendar.png')
+    .hover((e) => {
+        $('<p>')
+        .addClass('selector')
         .text(`Seleccione una fecha.`)
         .appendTo('body')
         .fadeIn();
@@ -14,5 +46,16 @@ $(()=>{
             top: e.pageY,
             left: e.pageX,
         });
+    })
+    .insertAfter('#date');
+}
+
+function sexo() {
+    $('#gender').selectmenu({
+        classes: {
+            "ui-selectmenu-menu": "highlight",
+            "ui-selectmenu-button-closed": "ui-corner-all",
+            "ui-selectmenu-button-open": "ui-corner-top",
+        }
     });
-});
+}
